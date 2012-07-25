2ca6d18c5860a16f88c7c8c2538092d50961fad0
Akram Hussein <akramhussein@gmail.com>
2012-07-25 12:31:52 +0100

    Added script to pre & post build to remove and create ppm and jpg folders Removed it from main.cpp

--- SimpleHDR/main.cpp
+++ SimpleHDR/main.cpp
@@ -21,19 +21,11 @@ int main( int argc, char* argv[] )
 {
 
     // Setup Video Source
-    
     FirewireVideo video = FirewireVideo(); // Simplified constructor
     
     VideoPixelFormat vid_fmt = VideoFormatFromString(video.PixFormat());
     const unsigned w = video.Width();
     const unsigned h = video.Height();
- 
-    // creates image folders if they don't already exist
-    // change to boost::filesystem when possible for cross compatibility
-    mkdir("ppm", 0755);
-    mkdir("jpg", 0755);
-    
-    video.PrintCameraReport();
     
     // Create Glut window
     pangolin::CreateGlutWindowAndBind("Main",w,h);
@@ -47,6 +39,8 @@ int main( int argc, char* argv[] )
     unsigned char* img = new unsigned char[video.SizeBytes()];
     bool over_exposed = true;
     
+    video.SetAutoAll();
+    
     for(int frame_number=0; !pangolin::ShouldQuit(); ++frame_number)
     {
         glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
@@ -54,19 +48,19 @@ int main( int argc, char* argv[] )
         
         if (over_exposed){
             cout << "Over" << endl;
-            video.SetExposureQuant(200);
+            //video.SetExposureQuant(200);
             over_exposed = false;
             cout << video.GetExposureQuant() << endl;
         }
         else {
             cout << "Under" << endl;
-            video.SetExposureQuant(800);
+            //video.SetExposureQuant(800);
             over_exposed = true;
             cout << video.GetExposureQuant() << endl;
         }
         
         // wait 1/30th of a second to sync change in settings and display
-        boost::this_thread::sleep(boost::posix_time::seconds(1/30));
+        //boost::this_thread::sleep(boost::posix_time::seconds(1/30));
         
         // Grab frame and save to JPG
         video.SaveFrame(frame_number, img, true, true);
